8863a76a8abebb5efa9f0d4f58e58ac9995b342b
Billi Ruiz <bcristianr@gmail.com>
2024-08-15 13:26:52 -0500

    Image Loader

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html',
+  styleUrl: './lazy-image.component.css'
+})
+export class LazyImageComponent implements OnInit {
+
+  public hasLoaded: boolean = false;
+
+  @Input()
+  public url!:string;
+
+  @Input()
+  public alt!:string;
+
+  ngOnInit(): void {
+    if(!this.url) throw new Error('URL property is required');
+  }
+
+  onLoad():void {
+    setTimeout(()=>{
+      this.hasLoaded = true
+    },1000);
+  }
+
+}
